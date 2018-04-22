53ff694d40a7f71cbff37becca3d38e04831264e
xucaihong <xucaihongleying365.com>
2018-04-22 14:59:06 +0800

    ‘填充内容’

--- local/demo.sh
+++ local/demo.sh
@@ -5,5 +5,11 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
+  echo '<!DOCTYPE>
+       <title>Hello</title>
+       <h1>Hi</h1>' >index.html
+  echo 'h1{color: red;}' >css/style.css
+  echo 'var string = "Hello World"
+        alert(string)' >js/main.js     
   exit
 fi
